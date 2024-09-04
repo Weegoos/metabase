1912412a23faee6878e3823c5f5a95c1815493cd
Weegoos <batyr.ashim05@mail.ru>
2024-09-04 15:55:31 +0500

    Metabase: The front-end part

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,13 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div style="height: 100%; width: 100%">
+    <iframe
+      src="http://localhost:3000/public/dashboard/044f8478-8cce-4ebd-a739-4631460e3d65"
+      frameborder="0"
+      style="height: 100vh; width: 100vw"
+    ></iframe>
+  </div>
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
-});
-</script>
+<script setup></script>
+
+<style></style>
